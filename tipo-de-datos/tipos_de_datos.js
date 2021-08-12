a24b8e57987085d08df019635383e3542110b677
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-12 14:38:12 -0500

    Create tipos_de_datos.js

---
+++ tipo-de-datos/tipos_de_datos.js
@@ -0,0 +1,21 @@
+ /*
+        * Tipos de datos en Js  
+        * Soy Sebastián </>
+        */
+
+        //String
+        let nombre = "Sebastián";
+        console.log(nombre, " En un string");
+
+        //Tipo de dato númerico
+        let numero = 7777;
+        console.log(numero);
+
+        //Objeto
+        let objeto = {
+                nombre : "Tian",
+                Apellido : "Contreras",
+                Edad : 19
+        }
+
+        console.log(objeto);
