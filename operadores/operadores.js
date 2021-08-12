17788e1231a7d35f7986ac2f85abf15293b964bb
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-12 15:47:03 -0500

    Create operadores.js

---
+++ operadores/operadores.js
@@ -0,0 +1,19 @@
+  /*
+        * Operadores en Js
+        * Sebastián Contreras </>
+        * + suma
+        * - resta
+        * * multiplicación
+        * / divisón
+        * % residuo
+        * ++ incremento
+        * -- decremento
+        */
+
+        let a = 3;
+        let b = 2;
+        let x = a + b;
+        console.log(x);
+
+        
+    </script>
