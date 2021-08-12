5dd33ce78a33825ddc8f3590aa0767a877b616ed
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-12 15:39:09 -0500

    Create sintaxis.js

---
+++ variables-sintaxis/sintaxis.js
@@ -0,0 +1,10 @@
+    <script>
+        /*
+        * Sintaxis para declarar variables en js  
+        * Sebastián Contreras </>
+        */
+
+        //Las variables de declaran con let para no tener errores de scope
+        let name = "Sebastián";
+        console.log(name);
+    </script>
