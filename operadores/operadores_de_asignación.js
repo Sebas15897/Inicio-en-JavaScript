28ae89f1065c221bc35ac874a470574b0077e694
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-12 15:58:36 -0500

    Create operadores_de_asignación.js

---
+++ operadores/operadores_de_asignación.js
@@ -0,0 +1,15 @@
+<script>
+        /*
+        * Operadores de asignación Js
+        * Sebastián Contreras </>
+        * = asignación
+        * += asignación compuesto
+        */
+
+        let a = 3;
+        let b = 2;
+        let x = a + b;
+        console.log(x);
+
+        
+    </script>
