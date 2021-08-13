46f4d0b2d19208cf557905f882e4301c61c5ede0
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-13 11:15:15 -0500

    Create if_else.js

---
+++ condicionales/if_else.js
@@ -0,0 +1,14 @@
+ <script>
+        /*
+        * if else
+        * Sebastián Contreras </>
+        * && and
+        * || regresa verdadero si cualquiera de los operando es true
+        */
+        
+        let condicion = true;
+
+        if ( condicion == true ) {
+            console.log("Es cierto");
+        }
+    </script>
