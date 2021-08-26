67b49e4d224ce8738dd41549ffa995ea25020ba0
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-08-26 14:33:21 -0500

    Add condición

--- Aplicación_Calculadora/js/app.js
+++ Aplicación_Calculadora/js/app.js
@@ -4,6 +4,9 @@ function sumar() {
     let formulario = document.forms['forma'];
     let a = formulario['operandoA'];
     let b = formulario['operandoB'];
-    let sumar = parseInt(a.value) + parseInt(b.value);
-    document.getElementById('resultado').innerHTML = `El resultado de la suma es: ${sumar}`;
+    let suma = parseInt(a.value) + parseInt(b.value);
+    document.getElementById('resultado').innerHTML = `El resultado de la suma es ${suma}`;
+    if(isNaN(suma)) {
+        document.getElementById('resultado').innerHTML = `No se han encontrado valores`;
+    }
 }
